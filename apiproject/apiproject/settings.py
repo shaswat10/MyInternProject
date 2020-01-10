6259e51a135d10353e20064726c64d425994b48c
shaswat <30467501+shaswat10@users.noreply.github.com>
2020-01-10 20:00:24 +0530

    Update settings.py

--- apiproject/apiproject/settings.py
+++ apiproject/apiproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ranuser'
+    'ranuser',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
